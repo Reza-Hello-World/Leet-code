66f0049cdb97bc70d33e2cf266ad6feb291a963e
Reza <reza00zehni@gmail.com>
2024-02-19 23:43:16 +0330

    commented code and start to write an adaptable code on leetcode

--- rotate image.js
+++ rotate image.js
@@ -1,21 +1,21 @@
-// allocate another matrix
+// this is not work on leetcode
+// let Image = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ]
+// let empthyArrays = [
+//     [],
+//     [],
+//     []
+// ]
+// empthyArrays.forEach(Element => {
+//     let helpMe = empthyArrays.indexOf(Element)
+//     for (let i = 0; i < Image.length; i++) {
+//         Element.unshift(Image[i][helpMe])
+//     }
+// })
+// console.log(empthyArrays)
 
-let Image = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-let empthyArrays = [
-    [],
-    [],
-    []
-]
-empthyArrays.forEach(Element => {
-    let helpMe = empthyArrays.indexOf(Element)
-    for (let i = 0; i < Image.length; i++) {
-        Element.unshift(Image[i][helpMe])
-    }
-})
-console.log(empthyArrays)
 
-// it will be true when you dont use another matrix
+// this is ok
